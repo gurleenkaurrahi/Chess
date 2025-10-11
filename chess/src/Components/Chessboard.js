ed7693220dc395421072f26d6f56cbee380f31b5
gurleenkaurrahi <gurleenkaurrahi03@gmail.com>
2025-10-11 18:58:25 -0400

    modified

---
+++ chess/src/Components/Chessboard.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Chessboard.css';
+
+const horizontalaxis = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+const verticalaxis = [1, 2, 3, 4, 5, 6, 7, 8];
+
+export default function Chessboard() {
+    return <div id= 'chessboard'></div>
+        
+}
