d10fa201a2c1b76dd71c3185225074614ffe0fb4
gurleenkaurrahi <gurleenkaurrahi03@gmail.com>
2025-10-26 17:56:29 -0400

    minor modifications

--- chess/src/Components/Chessboard.js
+++ chess/src/Components/Chessboard.js
@@ -5,6 +5,15 @@ const horizontalaxis = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
 const verticalaxis = [1, 2, 3, 4, 5, 6, 7, 8];
 
 export default function Chessboard() {
-    return <div id= 'chessboard'></div>
+    let board = [];
+
+    for (let i = 0; i < horizontalaxis.length; i++) {
+        for (let j = 0; j < verticalaxis.length; j++) {
+            board.push(<span>horizontal {horizontalaxis[i]}</span>)
+        }
+    return <div id= 'chessboard'>
+
+    </div>
         
+}
 }
