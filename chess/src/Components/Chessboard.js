56f4bff66a09a4e0aafba7b100778156769e5f70
gurleenkaurrahi <gurleenkaurrahi03@gmail.com>
2025-10-26 19:00:13 -0400

    final chessboard done

--- chess/src/Components/Chessboard.js
+++ chess/src/Components/Chessboard.js
@@ -2,18 +2,20 @@ import React from 'react';
 import './Chessboard.css';
 
 const horizontalaxis = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
-const verticalaxis = [1, 2, 3, 4, 5, 6, 7, 8];
+const verticalaxis = ['1', '2', '3', '4', '5', '6', '7', '8'];
 
 export default function Chessboard() {
     let board = [];
 
-    for (let i = 0; i < horizontalaxis.length; i++) {
-        for (let j = 0; j < verticalaxis.length; j++) {
-            board.push(<span>horizontal {horizontalaxis[i]}</span>)
+    for (let j = verticalaxis.length-1; j>=0; j--) {
+        for (let i = 0; i < horizontalaxis.length; i++) {
+            const number = j + i;
+            if (number % 2 === 0)
+                board.push(<div className='tile black-tile'></div>)
+            else
+            board.push(<div className='tile white-tile'></div>)
         }
-    return <div id= 'chessboard'>
+    }
+    return <div id= 'chessboard'>{board}</div>;
 
-    </div>
-        
-}
 }
